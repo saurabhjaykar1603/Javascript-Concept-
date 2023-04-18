67462b1bbaa65acb2a61b177a1dff51d0369cfc9
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-04-18 09:17:50 +0530

    Primitives Datatype with example

---
+++ Day 3 ( Primitives and Objects in JavaScript )/Primitives .js
@@ -0,0 +1,9 @@
+let a = null;  //object
+let b = 345;   //number
+let c = true; // can also be False  bulien
+let d = BigInt("567") //BigInt
+let e = "saurabh"  //string
+let f = Symbol(" i am nice symbol") //symbol
+let g = undefined   //undefined
+console.log(a, b, c, d, e, f, g);
+console.log(typeof g)
