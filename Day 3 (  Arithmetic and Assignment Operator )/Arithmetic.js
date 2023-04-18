44f6078d7e8f8ad011c15115a780ba48756445bb
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-04-18 09:00:14 +0530

    Arithmetic operator with example

---
+++ Day 3 (  Arithmetic and Assignment Operator )/Arithmetic.js
@@ -0,0 +1,21 @@
+    // assignment operators examples
+    const a = 20;
+    const b = 20;
+    const result1 = a + b;
+    document.write(result1 + "<br/><br/>");
+
+    const c = 20;
+    const d = 20;
+    const result2 = c - d;
+    document.write(result2 + "<br/><br/>");
+
+
+    const e = 20;
+    const f = 20;
+    const result3 = e / f;
+    document.write(result3 + "<br/><br/>");
+
+    const g = 20;
+    const h = 20;
+    const result4 = g * h;
+    document.write(result4 + "<br/><br/>");
