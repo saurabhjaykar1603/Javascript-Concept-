39807cbeb4dd5703e36b801bf7a09a23ee72841f
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-04-18 09:02:53 +0530

    Assignment  operator with example

---
+++ Day 3 (  Arithmetic and Assignment Operator )/Assignment.js
@@ -0,0 +1,29 @@
+      //  Current Balance
+      let balance = 500;
+      document.write("<br/> Current Balance" + balance + "<br/>")
+
+      // 15 rs. Added in Current Balance
+      // balance = balance + 15
+
+      balance += 15
+      document.write("<br/>New Balance" + balance + "<br/>")
+
+      // 15 rs. deduct in Current Balance
+      // balance = balance - 15
+      balance -= 15
+      document.write("<br/>New Balance" + balance + "<br/>")
+
+      // 15 rs. * in Current Balance
+      // balance = balance * 15
+      balance *= 15
+      document.write("<br/>New Balance" + balance + "<br/>")
+
+        // 15 rs. / in Current Balance
+      // balance = balance / 15
+      balance /= 15
+      document.write("<br/>New Balance" + balance + "<br/>")
+
+        // 15 rs. % 0f Current Balance
+      // balance = balance % 15
+      balance %= 15
+      document.write("<br/>New Balance" + balance + "<br/>")
