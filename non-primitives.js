3e26c8a7b973dd3933559018a1132436d67d100c
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-04-18 09:19:22 +0530

    non-primitives Datatype with example

---
+++ non-primitives.js
@@ -0,0 +1,9 @@
+  // in js Objects are non Primitives datatypes
+  const item = {
+    // look-up Dictionary
+    "sau": true,
+    "Shubham": false,
+    "kunal": 67,
+    "Rohan": undefined
+}
+console.log(item["sau"])
