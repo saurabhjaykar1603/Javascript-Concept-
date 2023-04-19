0e78703dc871bdaa1c596b71c3fe7234dbede560
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-04-19 09:03:03 +0530

    relational operator with example

---
+++ Day 4 ( relational or comparision operators )/rela.js
@@ -0,0 +1,47 @@
+// Relational Operators Or Comparision Operators
+// (1) == euals to
+// Two Variables with same Value
+let a = 10;
+b = 10;
+c = 10
+d = 5
+document.write(a == b);
+// tr
+document.write(c == d);
+// fl
+
+
+// (2) != not euals to
+// Two Variables with not same Value
+
+document.write(a != b);
+// fl
+document.write(c != d);
+// tr
+
+// (3) "<" Less than
+let e = 10;
+f = 11;
+g = 10;
+h = 5;
+document.write(e < f);
+// tr
+document.write(g < h);
+// fl
+
+// (4) ">" Greater  than
+let p = 10;
+q = 9;
+s = 10;
+v = 11;
+document.write(p > q);
+// tr
+document.write(s > v);
+// fl
+
+// (5) ">=" Greater  than or equals to
+// (6) "<=" Less  than or equals to
+
+
+
+
