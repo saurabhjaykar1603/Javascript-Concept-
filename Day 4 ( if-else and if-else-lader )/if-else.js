c4d74cbe4152515c1e9d2ce3586cb968943cd3ba
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-04-19 09:19:19 +0530

    if-else statement with example

---
+++ Day 4 ( if-else and if-else-lader )/if-else.js
@@ -0,0 +1,22 @@
+const age = parseInt(prompt("Enter Your Age: "));
+
+if(age>=18)
+{
+  document.write("You are eligible for voting...");
+}
+else
+{
+  document.write("You are not eligible for voting...");
+
+}
+
+// use if-else and write JS program to check if given number is possitive or megative
+// const number = parseInt(prompt(" Enter Any number: "));
+// if(number<0)
+// {
+//   document.write(" Negative Number");
+// }
+// else
+// {
+//   document.write("pogitive Number");
+// }
