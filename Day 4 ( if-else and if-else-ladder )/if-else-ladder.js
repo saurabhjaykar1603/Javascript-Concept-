124ef00ce76c768bc431a9e72e7c1f29b18afb6f
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-04-19 09:26:03 +0530

    if-else-ladder statement with example

---
+++ Day 4 ( if-else and if-else-ladder )/if-else-ladder.js
@@ -0,0 +1,30 @@
+const DAYnum = parseInt(prompt("Enter Day Number: "));
+
+if (DAYnum == 1) {
+    document.write("Monday")
+}
+
+else if (DAYnum == 2) {
+    document.write("Tuesday")
+}
+
+else if (DAYnum == 3) {
+    document.write("wednesday")
+}
+
+else if (DAYnum == 4) {
+    document.write("Thursday")
+}
+
+else if (DAYnum == 5) {
+    document.write("Friday")
+}
+else if (DAYnum == 6) {
+    document.write("Saturday")
+}
+else if (DAYnum == 7) {
+    document.write("Sunday")
+}
+else {
+    document.write(" Day Not Match")
+}
