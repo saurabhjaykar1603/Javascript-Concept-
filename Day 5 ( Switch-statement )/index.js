af46a290825220e2bcc9882f8d0e9b5813f053de
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-04-20 08:34:23 +0530

    Switch Statement with example

---
+++ Day 5 ( Switch-statement )/index.js
@@ -0,0 +1,32 @@
+switch (alphabet) {
+    case "a":
+        {
+            document.write("it is Vowel");
+            break;
+        }
+    case "e":
+        {
+            document.write("it is Vowel");
+            break;
+        }
+    case "o":
+        {
+            document.write("it is Vowel");
+            break;
+        }
+    case "i":
+        {
+            document.write("it is Vowel");
+            break;
+        }
+    case "u":
+        {
+            document.write("it is Vowel");
+            break;
+        }
+    default: {
+        document.write("it is consonant")
+    }
+
+}
+
