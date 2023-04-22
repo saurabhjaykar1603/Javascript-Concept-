b85de6393caed1f346dc2951f8fa59ec1b683723
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-04-22 09:34:42 +0530

    initial changes and square-root math function with example

---
+++ Day 7 ( Math Functions in JavaScript )/sqaur-root.js
@@ -0,0 +1,6 @@
+const num = parseFloat(prompt("Enter the Number"));
+// When you enter any number then it exucated there sqaur-root
+document.write("Actual Value: " + num + "<br/>");
+
+const result = Math.sqrt(num);
+document.write("Result: " + result);
