9c1ca31d8cd5acb44200588d95ef6f6489a12f16
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-04-22 09:27:03 +0530

    match.floor and match.ceil function with example

---
+++ Day 7 ( Math Functions in JavaScript )/ceil-floor.js
@@ -0,0 +1,10 @@
+const num = parseFloat(prompt("Enter The Value"));
+document.write("Actual Value:" + num);
+
+const ceilingValue = Math.ceil(num);
+document.write("<br/>ceilingValue:" + ceilingValue);
+
+const floorValue = Math.floor(num);
+document.write("<br/>floorValue: "+ floorValue)
+
+// used html file for example 
