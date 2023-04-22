39a45a5656860a4e6511f786aa7717aa8a3bd398
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-04-22 09:20:25 +0530

    match.abs function with example

---
+++ Day 7 ( Math Functions in JavaScript )/abs.js
@@ -0,0 +1,6 @@
+const num = parseFloat(prompt("Enter the Number"));
+// When you enter any -Negative Value it always exicuted  +Positive value
+console.log("Actual Value: " + num);
+
+const result = Math.abs(num);
+console.log("REsult: " + result);
