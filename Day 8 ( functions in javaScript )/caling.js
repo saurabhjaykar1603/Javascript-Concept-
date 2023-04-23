de604373330cd771b4fe53e1d1512e853bd4884b
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-04-23 15:53:32 +0530

    calling function with example

---
+++ Day 8 ( functions in javaScript )/caling.js
@@ -0,0 +1,11 @@
+
+
+
+// 2 no sum 
+
+function addNumber(num1, num2) {
+    const sum = num1 + num2;
+    console.log("<br/>" + num1 + "+" + num2 + "=" + sum);
+}
+addNumber(12, 12)
+addNumber(12, 13)
