182befa93ef59c898ef7b05a6fc8e064b2b424c2
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-04-28 09:06:24 +0530

    javascript logic created for class changing

---
+++ Day 12  Template String and Changing Class/class.js
@@ -0,0 +1,12 @@
+function makeDanger() {
+    const element = document.getElementById("div-container");
+    element.className = "danzer";
+}
+function makeWarning() {
+    const element = document.getElementById("div-container");
+    element.className = "warning";
+}
+function makeSuccess() {
+    const element = document.getElementById("div-container");
+    element.className = "success";
+}
