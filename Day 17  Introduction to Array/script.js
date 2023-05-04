e74cf29d821495bc495256d3910f366e4c037a27
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-04 09:02:34 +0530

    introduction to Array: individual value

---
+++ Day 17  Introduction to Array/script.js
@@ -0,0 +1,18 @@
+ // Array is like single Variables which can store multiples value
+
+        // Arrey is basket of vagitables
+
+        // 0   1   2   3   4   5   6 index....no.
+        // | 0 | 1 | 2 | 3 | 4 | 5 |  arrey boxes
+
+        // syntax of Array
+
+        const student = ["Pavan", "Saurabh", "Mahesh", "Avinash", "Aditya"];
+        console.log(student)
+
+        // how to Access indiviual value
+        console.log(student[0])
+        console.log(student[1])
+        console.log(student[2])
+        const laststudent = student[4];
+        console.log(laststudent)
