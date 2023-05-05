9c54b94be52a5ddf6749dec4f42949779c975ed5
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-05 20:44:44 +0530

    array fun: with for loop

---
+++ Day 18 Array Function using for loop/script.js
@@ -0,0 +1,9 @@
+ // how to send hello to everyone using loop function
+        
+        const students = ["Pavan", "Saurabh", "Mahesh", "Avinash", "Aditya" ,"yogita"];
+
+        for(let i=0; i<=6; i++){
+        console.log(`Hello ${students[i]}`);
+    }
+
+
