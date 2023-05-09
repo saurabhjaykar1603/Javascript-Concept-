35d937cc893aeef9633895a0d9ced2d0c65fd416
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-09 09:46:56 +0530

    Character count program js

---
+++ Day 22 Charactor counts/scrpit.js
@@ -0,0 +1,4 @@
+// let tweet = prompt("Compose your tweet")
+// let tweetCount = tweet.length
+// alert("YoubHave Written"+ tweetCount+ "character, You Have"+(140-tweetCount)+"character remaining.");
+//  document.write
