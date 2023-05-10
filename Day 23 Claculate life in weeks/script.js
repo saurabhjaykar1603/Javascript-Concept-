a8d16cc42571e2c1fc001be5002c9f6bfb8dd6ef
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-10 09:02:36 +0530

    simple js function who calculate our life in weeks

---
+++ Day 23 Claculate life in weeks/script.js
@@ -0,0 +1,7 @@
+function lifeInWeeks(age) {
+    let yearsRemaining = 90 - age;
+    let days = yearsRemaining * 365;
+    let weeks = yearsRemaining * 52;
+    let month = yearsRemaining * 12;
+}
+console.log()
