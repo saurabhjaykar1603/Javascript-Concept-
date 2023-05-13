d204f6051a6bf180f3133bba9623074d907edbd6
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-13 08:00:19 +0530

    love calculator usig math.randon func

---
+++ Day 25 Love Calculator/script.js
@@ -0,0 +1,20 @@
+prompt("What is your name");
+prompt("What is your crush name");
+let loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+console.log(loveScore);
+if (loveScore > 70) {
+
+    alert("Your Love Score is " + loveScore + " % You Love each other like Laila Majanu");
+}
+if (loveScore > 30 && loveScore <= 70) {
+
+    alert("Your Love Score is " + loveScore + " %");
+}
+if (loveScore <= 30) {
+
+    alert("Your Love Score is " + loveScore + "%" + " You ho Togeather like ice 🧊  and Fire 🔥");
+}
+else {
+
+}
