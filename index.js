ad0f1cfe23077db291b9537df8b20da8644323ae
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-21 09:15:13 +0530

    remove element from the end of array

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ const students = ["A","B","C","D"]
 console.log(students)
 students.push("E")
 console.log(students)
+
+// 3. pop()
+
+// To remove element from the end of array
+const Marks = [50,45,30,20,10]
+console.log(Marks)
+Marks.pop()
+console.log(Marks)
